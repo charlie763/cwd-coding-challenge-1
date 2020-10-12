4a3a97ac463184178498bffc80674f9d1bf6afcc
Alejandro Linan <jandrolinan@gmail.com>
2020-10-11 21:46:59 -0500

    fetch weather data

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
 class Home extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            weatherData: null
+        }
+    }
+
+    fetchWeatherData = () => {
+        fetch('http://dataservice.accuweather.com/currentconditions/v1/348308?apikey=BL84oxFsikLXeqZkAcPefEG8okzzcGzu&language=en-us')
+            .then(r => r.json())
+            .then(data => {
+                this.setState({
+                    ...this.state,
+                    weatherData: data[0]
+                })
+            })
+    }
+
+    weatherBox = () => {
+        console.log(this.state.weatherData)
+    }
+
+    componentDidMount(){
+        this.fetchWeatherData()
+    }
+
     render(){
         return(
             <section>
                 <h1>
                     This would be the Home!
+                    { this.weatherBox() }
                 </h1>
             </section>
         )
@@ -13,3 +40,7 @@ class Home extends React.Component {
 }
 
 export default Home;
+
+// fetch('http://dataservice.accuweather.com/currentconditions/v1/348308?apikey=BL84oxFsikLXeqZkAcPefEG8okzzcGzu&language=en-us')
+// .then(r => r.json())
+// .then(json => console.log(json))
