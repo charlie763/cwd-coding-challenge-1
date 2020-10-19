53d9997539e1ba391a59e1e62747281909f03d86
charlie763 <cwisoff@gmail.com>
2020-10-19 14:11:09 -0600

    fix linting issues

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -20,7 +20,7 @@ class Home extends React.Component{
         return(
           <div className="row justify-content-center py-3">
             <div className="card home-card mb-3 primary-background">
-              <img className="card-img-top" src={this.props.isDayTime ? sun : moon} alt="sun or moon image depending on time of day" />
+              <img className="card-img-top" src={this.props.isDayTime ? sun : moon} alt="sun or moon depending on time of day" />
               <div className="card-body">
                 <h5 className="text-center">Temperature</h5>
                 <p className="card-text text-center">{this.props.temperature.Imperial.Value}<img src={degree} alt="degree icon"/>{this.props.temperature.Imperial.Unit}</p>
