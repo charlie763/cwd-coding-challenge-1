39f47a697dcc4f1c4358647b05cc843096cd3405
charlie763 <cwisoff@gmail.com>
2020-10-19 10:46:02 -0600

    add Nav and Footer components to app

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './components/home/index'
 import About from './components/about/index'
 import Contact from './components/contact/index'
-
+import NavBar from './components/global/nav/index'
+import Footer from './components/global/footer/index'
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <Home />
-        </Route>        
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>        
-      </Switch>
-    </Router>
+    <>
+      <NavBar />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>        
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>        
+        </Switch>
+      </Router>
+      <Footer />
+    </>
   );
 }
 
