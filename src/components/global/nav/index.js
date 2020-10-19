49e0de3fc033fe36e2fdd5267aaddfa12a9f8ab5
charlie763 <cwisoff@gmail.com>
2020-10-19 10:59:58 -0600

    separate links in style

--- src/components/global/nav/index.js
+++ src/components/global/nav/index.js
@@ -3,14 +3,14 @@ import { NavLink } from 'react-router-dom'
 
 const NavBar = props => {
   return(
-    <nav id="navbar" className="navbar navbar-expand-lg navbar-light justify-content-between">
+    <nav id="navbar" className="navbar navbar-expand-lg navbar-light">
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-menu" aria-controls="nav-menu" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div>
-        <NavLink className="nav-text" to="/">Home</NavLink>
-      </div>
-      <div className="collapse navbar-collapse" id="nav-menu">
+      <div className="collapse navbar-collapse justify-content-between" id="nav-menu">
+        <div>
+          <NavLink className="nav-text" to="/">Home</NavLink>
+        </div>
         <ul className="navbar-nav">
           <li className="nav-item">
             <NavLink className="nav-text" to="/about">About</NavLink>
